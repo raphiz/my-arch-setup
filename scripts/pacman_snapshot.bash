51e096d4f2aebd7d4052a1f20704e85f2c2c1720
Raphael Zimmermann <raphael.zimmermann@ergon.ch>
2021-02-09 10:13:11 +0100

    Create a new snapshot in pacman hook instead of overriding STABLE

--- scripts/pacman_snapshot.bash
+++ scripts/pacman_snapshot.bash
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 set -eu
 
+
+NEW_SNAPSHOT_NAME=$(date +"%Y-%m-%d")
+if [[ -e "/.snapshots/$NEW_SNAPSHOT_NAME" ]] ; then
+    i=1
+    while [[ -e "/.snapshots/$NEW_SNAPSHOT_NAME""_$i" ]] ; do
+        (( i++ )) || true
+    done
+    NEW_SNAPSHOT_NAME="$NEW_SNAPSHOT_NAME""_$i"
+fi
 CURRENTLY_MOUNTED_SNAPSHOT=$(findmnt -n --target=/ -o OPTIONS | sed -E 's/.*subvol=\/@snapshots\/([^,]+*).*/\1/')
 
 # Don't do anything if not on EDGE
@@ -8,9 +17,9 @@ if [ "$CURRENTLY_MOUNTED_SNAPSHOT" != "EDGE" ]; then
     exit 0;
 fi
 
-# Backup edge as stable
+# Backup edge
 SCRIPT_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
-"$SCRIPT_PATH/backup_edge_as_stable.bash"
+"$SCRIPT_PATH/create_new_snapshot.bash" "$NEW_SNAPSHOT_NAME" "EDGE"
 
 # Remove pacman db lock
-rm /.snapshots/STABLE/var/lib/pacman/db.lck
+rm "/.snapshots/$NEW_SNAPSHOT_NAME/var/lib/pacman/db.lck"
